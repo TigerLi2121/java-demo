b327e447a35a03d156db5531b40ff9b7dc056ad5
liwulin <wlliq@isoftstone.com>
2018-02-09 15:58:58 +0800

    add file

---
+++ src/com/mm/File/FileUtils.java
@@ -0,0 +1,93 @@
+package com.mm.File;
+
+import java.io.*;
+import java.nio.file.*;
+
+/**
+ * @author: Shmily
+ * @date: 2018/2/9 13:43
+ **/
+public class FileUtils {
+
+    public static void main(String[] args) {
+        String filePath = "file/demo/test.txt";
+        deleteFile(filePath);
+        byte2File("哈哈呵呵".getBytes(), filePath);
+        byte2File2("嘿嘿".getBytes(), filePath);
+        try {
+            OutputStream outputStream = new FileOutputStream(new File("file/demo/demo.txt"));
+            file2OutputStream(outputStream, filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(file2Byte(filePath).length);
+    }
+
+    /**
+     * 字节转文件
+     * 文件不存在
+     * @param bytes
+     * @param fielPath
+     */
+    public static void byte2File(byte[] bytes, String fielPath) {
+        try {
+            Files.copy(new ByteArrayInputStream(bytes), Paths.get(fielPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 字节转文件
+     * 文件已存在
+     * @param bytes
+     * @param fielPath
+     */
+    public static void byte2File2(byte[] bytes, String fielPath) {
+        try {
+            Files.write(Paths.get(fielPath), bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 文件转字节
+     * @param filePath
+     * @return
+     */
+    public static byte[] file2Byte(String filePath){
+        byte[] result = null;
+        try {
+            result = Files.readAllBytes(Paths.get(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 文件转输出流
+     * @param outputStream
+     * @param filePath
+     */
+    public static void file2OutputStream(OutputStream outputStream, String filePath){
+        try {
+            Files.copy(Paths.get(filePath), outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除文件
+     * @param filePath
+     */
+    public static void deleteFile(String filePath){
+        try {
+            Files.deleteIfExists(Paths.get(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/mm/File/FilesTest.java
@@ -0,0 +1,48 @@
+package com.mm.File;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Shmily
+ * @date: 2018/2/9 10:33
+ **/
+public class FilesTest {
+    public static void main(String[] args) {
+        Path path = Paths.get("file/demo/demo.txt");
+        if (!Files.exists(path)) {
+            try {
+                Path dirPath = Files.createDirectories(Paths.get("file/demo"));
+                System.out.println("dirPath:" + dirPath.toString());
+                Path filePath = Files.createFile(path);
+                System.out.println("filePath:" + filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            List<String> poem = new ArrayList<>();
+            poem.add("水晶潭底银鱼跃");
+            poem.add("清徐风中碧竿横");
+            // 直接将多个字符串内容写入指定文件中
+            Files.write(path, poem, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+            // 使用Java 8新增的Stream API列出当前目录下所有文件和子目录
+            Files.list(Paths.get(".")).forEach(dirPath -> System.out.println(dirPath));
+            // 使用Java 8新增的Stream API读取文件内容
+            Files.lines(path, Charset.forName("UTF-8")).forEach(line -> System.out.println(line));
+            // 获取文件信息
+            BasicFileAttributes ra = Files.readAttributes(path, BasicFileAttributes.class);
+            System.out.println("FILE SIZE:" + ra.size());
+            System.out.println("FILE SIZE:" + Files.getAttribute(path, "size"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

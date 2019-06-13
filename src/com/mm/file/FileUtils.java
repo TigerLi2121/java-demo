909c8182560011b625e1a19cc8e1de5be71f94cb
lwl <83432087@qq.com>
2019-06-13 16:51:35 +0800

    del

--- src/com/mm/file/FileUtils.java
+++ src/com/mm/file/FileUtils.java
@@ -1,4 +1,4 @@
-package com.mm.File;
+package com.mm.file;
 
 import java.io.*;
 import java.nio.file.*;
---
+++ src/com/mm/nio/NioTest.java
@@ -0,0 +1,16 @@
+package com.mm.nio;
+
+import java.nio.channels.ByteChannel;
+import java.nio.channels.Channel;
+
+/**
+ * @author lwl
+ * @date 2019/5/5
+ */
+public class NioTest {
+
+    public static void main(String[] args) {
+        String s = "哈哈哈哈哈";
+
+    }
+}
